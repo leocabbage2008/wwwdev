ae61c582320eeaf279b95a430a8446637b6f7657
leocabbage2008 <70451398+leocabbage2008@users.noreply.github.com>
2021-07-17 20:06:49 -0400

    ??

--- se-4/class-7/script.js
+++ se-4/class-7/script.js
@@ -1,50 +1,57 @@
-function isArray(input){
+function isArray(input) {
     return Array.isArray(input);
 }
-function joinArray(input){
+
+function joinArray(input) {
     input.join("$");
 }
-function find(input, finding){
-    for(let i=0; i<input.length; i++){
-        if(finding==input[i]){
+
+function find(input, finding) {
+    for (let i = 0; i < input.length; i++) {
+        if (finding == input[i]) {
             return i;
         }
     }
 }
-function sum(input){
-    let accum=0;
-    for(i of input){
-        accum+=i;
+
+function sum(input) {
+    let accum = 0;
+    for (i of input) {
+        accum += i;
     }
     return accum;
 }
-function prod(input){
-    let accum=0;
-    for(i of input){
-        accum*=i;
+
+function prod(input) {
+    let accum = 0;
+    for (i of input) {
+        accum *= i;
     }
     return accum
 }
-function rmDup(input){
+
+function rmDup(input) {
     Array.from(new Set(input));
 }
-function leapYear(start, end){
-    let ret=[];
-    for(let i = start; i<end+1; i++){
-        if((i%4==0 && i%100!=0) || (i%400==0)){
+
+function leapYear(start, end) {
+    let ret = [];
+    for (let i = start; i < end + 1; i++) {
+        if ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0)) {
             ret.push(i);
         }
     }
     return ret;
 }
-function fibb(input){
-    let a=1;
-    let b=1;
+
+function fibb(input) {
+    let a = 1;
+    let b = 1;
     let a1;
-    for(let i = 0; i<input-1; i++){
-        a1=b;
-        b+=a;
-        a=a1;
+    for (let i = 0; i < input - 1; i++) {
+        a1 = b;
+        b += a;
+        a = a1;
     }
     return b;
 }
